4c89125d649115d7129916795d5ad72a35d0afe3
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-06-13 18:03:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// function login() {
+//    let username = document.querySelector("input").value;
+
+//    alert(username);
+// }
+function singup() {
+    let isAgree = document.querySelector("#terms").checked;
+
+    if (isAgree == true 
+    {
+        alert("congratulation ")
+    })
+}
